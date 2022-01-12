e57c6702b96e26fd5f8bd1ee866e77f91b56d8d6
Rehana Mulla <rehana0mulla786@gmail.com>
2022-01-12 11:27:59 +0530

    Hibernate Examples

---
+++ src/com/xworkz/tour/TouristRunner.java
@@ -0,0 +1,15 @@
+package com.xworkz.tour;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TouristRunner {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory=
+				Persistence.createEntityManagerFactory("com.xworkz.tour");
+		System.out.println(entityManagerFactory);
+
+	}
+
+}
---
+++ src/com/xworkz/tour/dao/TouristDAOImpl.java
@@ -0,0 +1,12 @@
+package com.xworkz.tour.dao;
+
+import com.xworkz.tour.entity.TouristEntity;
+
+public class TouristDAOImpl implements TouristDAO{
+
+	@Override
+	public void put(TouristEntity entity) {
+		
+	}
+
+}
